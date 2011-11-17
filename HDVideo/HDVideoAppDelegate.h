5457215f3cf25d45274d8db2996ecebac4c75027
Perry Lei <perry.lei@gmail.com>
2011-11-17 11:01:32 +0800

    Initial Commit

---
+++ HDVideo/HDVideoAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  HDVideoAppDelegate.h
+//  HDVideo
+//
+//  Created by Perry on 11-11-17.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class HDVideoViewController;
+
+@interface HDVideoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet HDVideoViewController *viewController;
+
+@end
---
+++ HDVideo/HDVideoViewController.h
@@ -0,0 +1,15 @@
+//
+//  HDVideoViewController.h
+//  HDVideo
+//
+//  Created by Perry on 11-11-17.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDVideoViewController : UIViewController {
+    
+}
+
+@end
